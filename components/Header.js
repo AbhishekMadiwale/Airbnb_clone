dd4f2be3849edb535fe6f19a90990bd905931f6e
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2026-09-01 22:43:11 +0530

    Header completed

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import {
+  MagnifyingGlassIcon,
+  GlobeAltIcon,
+  Bars4Icon,
+  UserCircleIcon,
+} from "@heroicons/react/16/solid";
 
 function Header() {
   return (
@@ -17,13 +22,25 @@ function Header() {
       </div>
 
       {/* Middle */}
-      <div className="flex justify-between border-gray-600 border-2 rounded-full align-middle ">
-        <input type="text" placeholder="Search for stay" />
-        <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-1 " />
+      <div className="flex items-center justify-between border-gray-400 border-2 rounded-full p-3 m-2 md:shadow-sm">
+        <input
+          className="pl-2 bg-transparent outline-transparent"
+          type="text"
+          placeholder="Search for stay"
+        />
+        <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full cursor-pointer p-1 md:mx-2" />
       </div>
 
       {/* Right */}
-      <div></div>
+      <div className="flex space-x-4 items-center justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6" />
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full border-gray-300">
+          <Bars4Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 }
