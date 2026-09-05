6848da1157fa93d154e8adbc843389591e167a94
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2026-09-05 13:20:09 +0530

    API call is done getting location for now

--- app/page.js
+++ app/page.js
@@ -1,11 +1,28 @@
 import Header from "../components/Header";
 import Banner from "@/components/Banner";
 
-export default function Home() {
+export const revalidate = 3600;
+
+export default async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json(),
+  );
+
   return (
     <div className="">
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* pull some data */}
+          {exploreData.map((item) => (
+            <h1 key={item.id}>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   );
 }
